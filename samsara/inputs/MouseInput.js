c3b8d1d5edb178f822b1af6e2c110f3d5c508b35
David Valdman <dmvaldman@gmail.com>
2016-07-23 11:16:36 -0400

    End events pushed to dirtyQueue

--- samsara/inputs/MouseInput.js
+++ samsara/inputs/MouseInput.js
@@ -4,6 +4,7 @@ define(function(require, exports, module) {
     var EventHandler = require('../events/EventHandler');
     var OptionsManager = require('../core/_OptionsManager');
     var SimpleStream = require('../streams/SimpleStream');
+    var dirtyQueue = require('../core/queues/dirtyQueue');
 
     var MINIMUM_TICK_TIME = 8;
     var _now = Date.now;
@@ -231,7 +232,10 @@ define(function(require, exports, module) {
         if (!this._down) return false;
 
         this._payload.event = event;
-        this._eventOutput.emit('end', this._payload);
+
+        dirtyQueue.push(function(){
+            this._eventOutput.emit('end', this._payload);
+        }.bind(this));
 
         this._prevCoord = undefined;
         this._prevTime = undefined;
